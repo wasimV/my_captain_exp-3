886c351283ff5fe19bca66fe62200054d09c544d
wasimV <72549954+wasimV@users.noreply.github.com>
2021-02-21 20:17:59 +0530

    Create MainActivity2.java

---
+++ MainActivity2.java
@@ -0,0 +1,25 @@
+package com.wasim.facebooklogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button sign_up = findViewById(R.id.sign_up);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
